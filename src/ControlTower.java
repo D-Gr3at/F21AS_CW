3075a70d094e327e868a6694904cc18f5a461855
carl <cn2013@hw.ac.uk>
2022-02-22 02:11:47 +0000

    add(Control Tower && GPSCoordinate) Class definitions

--- src/ControlTower.java
+++ src/ControlTower.java
@@ -1,2 +1,33 @@
-public class ControlTower {
+/**
+ * Describe the Control Tower class
+ * @author  Carl-Olivier N'Diaye (cn2013@hw.ac.uk)
+ * @version v1.0
+ * @since   07 Feb 2022
+ */
+
+public final class ControlTower {
+    private GPSCoordinate coordinates;
+
+    public ControlTower(GPSCoordinate newCoordinates) {
+        coordinates.setLongitude(newCoordinates.getLongitude());
+        coordinates.setLatitude(newCoordinates.getLatitude());
+    }
+
+    public ControlTower(String longitude, String latitude) {
+        coordinates = new GPSCoordinate(longitude, latitude);
+    }
+
+    public GPSCoordinate getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(GPSCoordinate newCoordinates) {
+        coordinates.setLongitude(newCoordinates.getLongitude());
+        coordinates.setLatitude(newCoordinates.getLatitude());
+    }
+
+    public void setCoordinates(String longitude, String latitude) {
+        coordinates.setLongitude(longitude);
+        coordinates.setLatitude(latitude);
+    }
 }
