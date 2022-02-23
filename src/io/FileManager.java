57975ba646c0bf32ae4c1887f2288aa1cfaf0eac
Andrieux, Aurelien <apa3@hw.ac.uk>
2022-02-23 14:08:57 +0000

    First prototype for a generateReport method + various fixes

--- src/io/FileManager.java
+++ src/io/FileManager.java
@@ -191,6 +191,69 @@ public class FileManager {
 		}
 				
 		System.out.println("New list from file: " + FileManager.loadAirports());
+	}
+	
+	//Write report of all flights
+	//Replace all Integers things by the Flight Class
+	//Need testing once we have all the other classes
+	public static void generateReport(HashMap<String, Integer> flightList) {
+		
+		/**Structure of the file:
+			Company1Code:
+				- Number of flights:X
+				- Total number of kilometers:X
+				- Average fuel consumption: X
+				- Average CO2 emission: X
+			Company2Code:
+			...
+		**/
+		
+		
+		//Copy the flightList in a separate HashMap in order to modify it later without impact flightList
+		HashMap<String, Integer> tempFlightList = new HashMap<String, Integer>();
+		flightList.forEach((code, flight) -> {
+			tempFlightList.put(code, flight);
+		});
+		
+		
+		
+		
+		try {
+			FileWriter fw = new FileWriter("Report.txt");
+						
+			//get first element's company code
+			
+			while(tempFlightList.size() != 0) {
+				String airlineCode = (String) tempFlightList.keySet().toArray()[0]; //REMOVE the serial number
+				fw.write("COMPANY NAME HERE" + System.lineSeparator());
 
+
+				/**Concerning this array:
+					-0 is the number of flight
+					-1 is the total number of kilometers
+					-2 is the average fuel consumption
+					-3 is the average CO2 emission 
+				**/
+				int[] companyInformation = new int[4];
+				tempFlightList.forEach((code, flight) -> {
+						//ADD if flight.flightCode contains airlineCode
+						companyInformation[0] += 1;
+						companyInformation[1] += 0; //Replace by the number of kilometers of the flight
+						companyInformation[2] += 0; //Replace by the fuel consumption of the flight
+						companyInformation[3] += 0; //Replace by the CO2 emission of the flight
+				});
+				
+				fw.write("	Number of flights: " + companyInformation[0] + System.lineSeparator());
+				fw.write("	Total number of kilometers: " + companyInformation[1] + System.lineSeparator());
+				fw.write("	Average Fuel consumption: " + companyInformation[2] + System.lineSeparator());
+				fw.write("	Average CO2 emission: " + companyInformation[3] + System.lineSeparator());
+				tempFlightList.entrySet().removeIf(entry -> entry.getKey().contains(airlineCode));
+			}
+			
+			fw.close();
+		}
+		catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
 	}
 }
