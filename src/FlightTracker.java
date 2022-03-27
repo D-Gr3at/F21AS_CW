535d18daf08cc0751fcd7ce6a281dff430a61cfd
ib2008 <ib2008@hw.ac.uk>
2022-03-27 09:28:57 +0000

    updated code

--- src/FlightTracker.java
+++ src/FlightTracker.java
@@ -1,22 +1,13 @@
-import java.util.LinkedList;
-
-import flightressources.ControlTower;
-import gui.*;
-import io.FileManager;
-import flightressources.GPSCoordinate;
-
-import threads.FlightRunnable;
-
 public class FlightTracker {
 
-	public static void main(String[] args) {
-		Gui.startGUI();
-				
-		Thread test = new Thread(new FlightRunnable());
-		test.start();
-		
-		
-	}
+//	public static void main(String[] args) {
+//		Gui.startGUI();
+
+//		Thread test = new Thread(new FlightRunnable());
+//		test.start();
+
+
+//	}
 
 	
 	
